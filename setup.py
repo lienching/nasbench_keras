c5e0c99d2d809970795d2da3970b409f06f7ae6b
evgps <evgps@ya.ru>
2020-02-15 18:58:58 +0300

    minor fix

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   name = 'nasbench_keras',         # How you named your package folder (MyLib)
   packages = ['nasbench_keras'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='Apache 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='MIT License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'NAS-Bench 101 to Tensorflow 2.0 (tf.keras) converter',   # Give a short description about your library
   author = 'evgps',                   # Type in your name
   author_email = 'evgps@ya.ru',      # Type in your E-Mail
@@ -27,7 +27,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache 2.0',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3'      #Specify which pyhton versions that you want to support
   ],
 )
